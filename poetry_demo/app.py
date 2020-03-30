5e42af3c31bdcf4fe57545dd261ebd4dbb83caf6
Loh Wan Jing <lohwanjing@gmail.com>
2020-03-30 17:51:25 +0800

    added swagger

--- poetry_demo/app.py
+++ poetry_demo/app.py
@@ -1,14 +1,29 @@
 import logging.config
 import os
-from flask import Flask, Blueprint
+from flask import Flask
+from flask_cors import CORS
+from flask_restful_swagger_2 import swagger, get_swagger_blueprint
+from poetry_demo.views_blueprint import get_user_resources
 from poetry_demo import settings
-from poetry_demo.api.image.endpoints.persons import ns as persons_namespace
-from poetry_demo.api.restplus import api
+
 
 app = Flask(__name__)
+CORS(app)
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
+
+
+@app.route('/')
+def index():
+    return """<head>
+    <meta http-equiv="refresh" content="0; url=http://petstore.swagger.io/?url=http://localhost:5000/api/swagger.json" />
+    </head>"""
+
+
+def auth(api_key, endpoint, method):
+    # Space for your fancy authentication. Return True if access is granted, otherwise False
+    return True
 
 
 def configure_app(flask_app):
@@ -22,10 +37,20 @@ def configure_app(flask_app):
 def initialize_app(flask_app):
     configure_app(flask_app)
 
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
-    api.add_namespace(persons_namespace)
-    flask_app.register_blueprint(blueprint)
+    docs = []
+
+    swagger.auth = auth
+    # Get user resources
+    user_resources = get_user_resources()
+
+    # Retrieve and save the swagger document object (do this for each set of resources).
+    docs.append(user_resources.get_swagger_doc())
+
+    # Register the blueprint for user resources
+    app.register_blueprint(user_resources.blueprint)
+
+    # Prepare a blueprint to server the combined list of swagger document objects and register it
+    app.register_blueprint(get_swagger_blueprint(docs, '/api/swagger', title='Example', api_version='1'))
 
 
 def main():
